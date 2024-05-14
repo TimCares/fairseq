16b2e3534384461534c6abb1268dd5e825089077
Tim Cares <peep-kestrel-0t@icloud.com>
2024-05-14 13:14:46 +0200

    Adjusted setup

--- make.sh
+++ make.sh
@@ -1,8 +1,14 @@
 #!bin/bash
 
-conda create -n fairseq python=3.8 ipython
+apt install python3.8
+apt install python3.8-distutils
 
-conda activate fairseq
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 2
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.8 get-pip.py
+rm get-pip.py
 
 pip install --editable ./
 
